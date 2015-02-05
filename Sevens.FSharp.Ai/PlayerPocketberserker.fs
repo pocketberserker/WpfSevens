aeb5c93dc7b075b7a0bd3593774600d1a818e940
pocketberserker <pocketberserker@gmail.com>
2015-02-05 23:07:24 +0900

    様子見しながらテキトーにだす

--- Sevens.FSharp.Ai/PlayerPocketberserker.fs
+++ Sevens.FSharp.Ai/PlayerPocketberserker.fs
@@ -1,10 +1,17 @@
 ﻿namespace Sevens.FSharp.Ai
 
 open Sevens.Interfaces
+open Sevens.Core
 
 type PlayerPocketberserker() =
+  let passCount = ref 0
   interface IPlayer with
     member __.GetPalyerName() = "もみあげ"
     member __.GetPalyerImageName() = "pocketberserker.png"
     member __.GetPutCard(playerCards, putCards) =
-      null
+      let cs = Table.GetPutPossibleCards(playerCards, putCards)
+      if Seq.isEmpty cs || !passCount < 1 then
+        incr passCount
+        null
+      else
+        Seq.head cs
